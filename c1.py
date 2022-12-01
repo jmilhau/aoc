e0b1daa6b3b0ff3699cff4fc883cb591a0daa41a
Jérémy Milhau <jmilhau@redhat.com>
2022-12-01 17:02:18 +0100

    first commit

---
+++ c1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+with open("i1a.txt") as file:
+    lines = file.readlines()
+
+c = 0
+elves = []
+for l in lines:
+    if l == "\n":
+        elves.append(c)
+        c = 0
+    else:
+        c += int(l[:-1])
+
+print(max(elves))
+
+
+with open("i1b.txt") as file:
+    lines = file.readlines()
+
+c = 0
+elves = []
+for l in lines:
+    if l == "\n":
+        elves.append(c)
+        c = 0
+    else:
+        c += int(l[:-1])
+
+elves.sort(reverse=True)
+print(sum(elves[0:3]))
