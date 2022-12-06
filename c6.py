f5abf3f02d52481f2e518389e24b98a709bf86a7
Jérémy Milhau <jmilhau@redhat.com>
2022-12-06 09:29:05 +0100

    Day 6

---
+++ c6.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Input modified to only contain move lines
+with open("inputs/i6a.txt") as file:
+    lines = file.readlines()
+l = lines[0]
+
+for i in range(4,len(l)):
+    if len(l[i-4:i]) == len(set(l[i-4:i])):
+        print(i)
+        break
+
+for i in range(14,len(l)):
+    if len(l[i-14:i]) == len(set(l[i-14:i])):
+        print(i)
+        break
